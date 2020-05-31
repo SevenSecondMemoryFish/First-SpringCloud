3c4a1faf62d959b744902540b029d5df7f7cdaf7
wsj <593216709@qq.com>
2020-05-31 18:43:41 +0800

    eureka client 服务发现

--- cloud-provider-payment8001/src/main/java/com/seven/second/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/seven/second/springcloud/controller/PaymentController.java
@@ -5,9 +5,15 @@ import com.seven.second.springcloud.entities.Payment;
 import com.seven.second.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -17,6 +23,14 @@ public class  PaymentController {
 
     @Value("${server.port}")
     private String servicePort;
+
+    @Value("${spring.application.name}")
+    private String serviceName;
+
+    /** 服务发现*/
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @PostMapping(value = "payment/create")
     public CommentResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
@@ -31,5 +45,29 @@ public class  PaymentController {
         } else {
             return new CommentResult(500,"未查询到数据",null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public CommentResult discovery() {
+
+        /// 查询注册中心服务提供者
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("service: " + service);
+        }
+
+        /// 根据serviceId查询服务提供者信息
+        List<ServiceInstance> instances = discoveryClient.getInstances(serviceName.toUpperCase());
+        List list = new ArrayList<Map>();
+        HashMap map = null;
+        for (ServiceInstance instance : instances) {
+            map = new HashMap();
+            map.put("name", instance.getInstanceId());
+            map.put("host", instance.getHost());
+            map.put("port",instance.getPort());
+            map.put("url",instance.getUri());
+            list.add(map);
+        }
+        return new CommentResult(200,"查询成功",list);
     }
 }
