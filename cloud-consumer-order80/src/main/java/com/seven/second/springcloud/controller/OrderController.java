d9f43a7ba4bf7994ac4bb6bbb4cc5c9a95bca72d
wsj <593216709@qq.com>
2020-06-09 21:45:10 +0800

    Ribbon

--- cloud-consumer-order80/src/main/java/com/seven/second/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/seven/second/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.seven.second.springcloud.controller;
 import com.seven.second.springcloud.entities.CommentResult;
 import com.seven.second.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,11 +25,22 @@ public class OrderController {
     @GetMapping("/consumer/payment/create")
     public CommentResult<Payment> create(Payment payment) {
         log.info("我试测试");
-        return  restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommentResult.class);
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommentResult.class);
     }
 
     @GetMapping("/consumer/payment/get/{id}")
     public CommentResult<Payment> getPaymentById(@PathVariable("id") Long id) {
-        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommentResult.class);
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommentResult.class);
+    }
+
+    @GetMapping("/consumer/entity/get/{id}")
+    public CommentResult<Payment> getPaymentEntity(@PathVariable("id") Long id) {
+        /// entity 可以获取到更多响应数据，比如header statuscode等
+        ResponseEntity<CommentResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommentResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommentResult<>(400, "未查询到数据");
+        }
     }
 }
