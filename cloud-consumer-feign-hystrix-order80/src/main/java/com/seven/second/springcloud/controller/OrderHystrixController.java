e8c27494b1ce8f2bcc3ccc1ad2996ddcc6b8fc5c
wsj <593216709@qq.com>
2020-06-18 15:04:39 +0800

    全局的降级方法

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/seven/second/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/seven/second/springcloud/controller/OrderHystrixController.java
@@ -1,5 +1,6 @@
 package com.seven.second.springcloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.seven.second.springcloud.service.PaymentFeignService;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class OrderHystrixController {
 
     @Resource
@@ -20,6 +24,11 @@ public class OrderHystrixController {
         return paymentFeignService.paymentInfo_OK(id);
     }
 
+    /**
+     * 个别处理fallback方法
+     * @param id
+     * @return
+     */
     @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHander", commandProperties = {
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
     })
@@ -28,7 +37,19 @@ public class OrderHystrixController {
         return paymentFeignService.paymentInfo_Timeout(id);
     }
 
+    @HystrixCommand
+    @GetMapping(value = "/consumer/payment/hystrix/fallback/{id}")
+    public String paymentInfo_DefaulFallback(@PathVariable("id") Long id) {
+        List list = new ArrayList();
+        list.get(0);
+        return  paymentFeignService.paymentInfo_Timeout(id);
+    }
+
     public String paymentInfo_TimeoutHander(Long id) {
         return "我是80 order 服务。系统出错了。没办法了，自己看着办吧";
+    }
+
+    public String defaultFallback() {
+        return "我是全局的降级方法，你自己看着办班";
     }
 }
