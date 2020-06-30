91f5945e0f9bff4c6c9ac2f575098c631494b246
wsj <593216709@qq.com>
2020-06-30 22:14:05 +0800

    手动触发config service 修改后通知config client

--- cloud-config-client-3355/src/main/java/com/sevensecond/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/sevensecond/springcloud/controller/ConfigClientController.java
@@ -1,10 +1,17 @@
 package com.sevensecond.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+/** 刷新
+ * config service 更改后，还需要手动触发一下，发送一个post请求  http://localhost:3355/actuator/refresh
+ * curl -X POST http://localhost:3355/actuator/refresh
+ * localhost:3355  标识微服务config-client 的域名
+ * */
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.info}")
