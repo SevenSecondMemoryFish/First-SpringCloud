f399911a5007e27eb04e81c8ef8d459a047b8cee
wsj <593216709@qq.com>
2020-07-04 18:15:39 +0800

    修改bug

--- cloudalibaba-provider-payment9001/src/main/java/com/sevensecond/springcloud/controller/PaymentController.java
+++ cloudalibaba-provider-payment9001/src/main/java/com/sevensecond/springcloud/controller/PaymentController.java
@@ -1,15 +1,24 @@
 package com.sevensecond.springcloud.controller;
 
 import cn.hutool.core.util.IdUtil;
+import com.seven.second.springcloud.entities.CommentResult;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
-    public String getServerPort() {
-        return "server port: " + serverPort + "uuid: " + IdUtil.simpleUUID();
+    @GetMapping(value = "/nacos/port")
+    public CommentResult<Map> getServerPort() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("port", serverPort);
+        hashMap.put("uuid", IdUtil.simpleUUID());
+        return new CommentResult<Map>(200, "成功", hashMap);
     }
 }
